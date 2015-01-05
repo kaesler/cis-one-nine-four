dde7f9f4ebc40c7588d84b69fdc761bf4fd1ff6f
Kevin Esler <kevin.esler@gmail.com>
2015-01-05 10:15:02 -0500

    Hanoi 4 peg initial solution

--- Homework1/Hanoi.hs
+++ Homework1/Hanoi.hs
@@ -12,3 +12,20 @@ hanoi 0 _ _ _ = []
 hanoi 1 src dst _ = [(src, dst)]
 hanoi n src dst other = (hanoi (n - 1) src other dst) ++ [(src, dst)] ++ (hanoi (n - 1) other dst src)
 
+-- 4 peg version
+hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
+hanoi4 0 _ _ _ _ = []
+hanoi4 1 src dst _ _ = [(src, dst)]
+hanoi4 2 src dst alt1 _ = [(src, alt1), (src, dst), (alt1, dst)]
+-- hanoi4 3 src dst alt1 alt2 = [(src, alt2), (src, alt1), (src, dst), (alt1, dst), (alt2, dst)]
+-- n > 2
+-- so move all but bottom 2 to alt1
+--    move bottom two to dst without using alt1
+--      src alt2
+--      src dst
+--      alt2 dst
+--    move all at alt1 over to dst
+hanoi4 n src dst alt1 alt2 =
+  (hanoi4 (n - 2) src alt1 dst alt2)
+   ++ [(src, alt2), (src, dst), (alt1, dst)]
+   ++ (hanoi4 (n - 2) alt1 dst alt2 src)
